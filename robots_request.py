61f6d7d6e9d3e22c573fe59cb92cf0ea2836f5b8
Harshit Raj Singh <harshitrajsingh.hrs@gmail.com>
2021-10-27 12:55:16 +0530

    Update robots_request.py

--- robots_request.py
+++ robots_request.py
@@ -1,8 +1,11 @@
 import requests
+from furl import furl
 import re 
 
 urls = ['https://www.breachlock.com/', 'https://www.breachlock.com/how-it-works/','http://testasp.vulnweb.com/showforum.asp?id=2','http://testasp.vulnweb.com/']
 
+
+#usign regex
 #get base url 
 regex = re.compile(r'((http:\/\/|https:\/\/)?(www\.)?[\w.]*\.?com)', re.IGNORECASE)
 
@@ -21,3 +24,21 @@ for url in set(base_urls):
         print(f"\nThe robots.txt file content of {url} is \n {content}\n")
     except:
         print('\trobots.txt does not exist')
+
+#alternate way 
+
+origin_urls = list()
+for url in urls:
+    origin_url = furl(url).origin
+    if origin_url not in origin_urls:
+        origin_urls.append(origin_url)
+
+for url in origin_urls:
+    robot_url = f"{url}/robots.txt"
+    print(robot_url)
+    try:
+        resp = requests.get(robot_url)
+        content = resp.text
+        print(f"\n The robots.txt file content of {url} is \n{content}\n")
+    except:
+        print(f"\t robots.txt does not exist for {url}")
